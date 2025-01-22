7b73ea28c8b2a6a6837160e515ff39f2a65d8b01
ReaLac5 <realackovic5@gmail.com>
2025-01-22 20:03:31 +0100

    git

--- html/recommendations.js
+++ html/recommendations.js
@@ -40,23 +40,39 @@ document.addEventListener("DOMContentLoaded", () => {
   
     // Stranice s njihovim stvarnim sadržajem
     let pages = [
-      {
-        name: "/home",
-        content: "O meni\n\nTeo Matijašić\n“Stvaram desktop/web i mobilne aplikacije s jednostavnim i privlačnim sučeljem.”",
-      },
-      {
-        name: "/products",
-        content: "Pregled proizvoda\nOvdje možete pregledati sve proizvode koje nudimo.",
-      },
-      {
-        name: "/about",
-        content: "Informacije o meni\nStudent informatike i tehnologije s velikom strašću prema razvoju aplikacija.",
-      },
-      {
-        name: "/contact",
-        content: "Kontaktirajte nas putem emaila ili telefona za više informacija.",
-      },
-    ];
+        {
+          name: "/home",
+          content: "O meni\n\nTeo Matijašić\n“Stvaram desktop/web i mobilne aplikacije s jednostavnim i privlačnim sučeljem.” Putem ovoga web sjedišta, detaljnije me možeš upoznati, otkriti moje specijalnosti i vještine, pregledati moje projekte i radove ili me kontaktirati za moguću suradnju."
+        },
+        {
+          name: "/about",
+          content: "Kratke informacije o meni!\n\nTeo Matijašić\n“Sve projekte, koje sam izradio za fakultet, karakterizira jednostavnost, praktičnost i usmjerenost na korisnika.”\n\nOPĆENITO\nJa sam student Fakulteta informatike i digitalnih tehnologija u Rijeci koji voli učiti nove stvari i biti uporan u savladavanju novih vještina. Moji radovi obuhvaćaju područja istraživanja korisnika, UI/UX dizajna i razvoja različitih vrsta aplikacija."
+        },
+        {
+          name: "/projects",
+          content: "Moji projekti i radovi!\n\nWeb aplikacija za stručnu praksu\nPodručje:Web razvoj, Backend-Middle-Frontend, Web aplikacija\nProblem: Razvoj cjelokupne web aplikacije za evidenciju stručne prakse na fakultetima\nRezultat: Kreirana funkcionalna web aplikacija s brojnim mogućnostima, spremno za produkciju."
+        },
+        {
+          name: "/project1",
+          content: "Web aplikacija (PostgreSQL/.NET/React)\nAutor: Teo Matijašić\nOpis Projekta\nOvaj projekt je podrazumijevao izradu web aplikacije za evidenciju stručne prakse na fakultetu. Pritom su kreirani svi osnovni dijelovi web aplikacije, od baze podataka u PostgreSQL-u, srednjeg dijela u C# i .NET-u te frontenda u React-u."
+        },
+        {
+          name: "/project2",
+          content: "Dizajn aplikacije Servisly\nAutor: Teo Matijašić\nOpis Projekta\nOvaj projekt je podrazumijevao izradu cjelokupnoga dizajna aplikacije za online naručivanje za servis automobila. Provedene su sve faze UI i UX dizajna, uključujući istraživanje korisnika, analize konkurencije, wireframe, mockup, prototip i testiranje s korisnicima."
+        },
+        {
+          name: "/project3",
+          content: "Aplikacija za putne naloge\nAutor: Teo Matijašić\nOpis Projekta\nOvaj projekt je podrazumijevao izradu cjelokupne aplikacije za evidenciju putnih naloga u Clarionu. Temelj projekta bio je papirnati putni nalog na temelju kojega je kreiran model podataka koji je razrađen u cjelokupnu aplikaciju."
+        },
+        {
+          name: "/contact",
+          content: "Kontaktiraj me!\nTeo Matijašić\nBroj telefona: 091 954 6450\nEmail: teo12matijasic@gmail.com\nDruštvene mreže:\nPošalji mi Email!"
+        },
+        {
+          name: "/cv",
+          content: "Moj životopis (CV)\nPreuzmi CV"
+        }
+      ];
   
     // Dohvaćanje preporuka
     let recommendations = getRecommendations(userHistory, pages);
