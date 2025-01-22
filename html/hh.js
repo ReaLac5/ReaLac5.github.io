8174a85c26be1ff4c4fa5759233d00f6863598f0
ReaLac5 <realackovic5@gmail.com>
2025-01-22 15:05:30 +0100

    git

--- html/hh.js
+++ html/hh.js
@@ -337,6 +337,8 @@ function renderCharts(chartData) {
               weight: 'bold',
             },
             formatter: (value, context) => {
+              console.log("Labels: ", labels);
+              console.log("Values: ", values);
               const total = context.dataset.data.reduce((acc, curr) => acc + curr, 0);
               const percentage = ((value / total) * 100).toFixed(2); // Postotak
               return `${percentage}%`; // Možeš zamijeniti s `value` za broj
