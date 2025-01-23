b24dfc1b728da8d6342ac31a0a40f8400da4caf7
ReaLac5 <realackovic5@gmail.com>
2025-01-23 11:56:36 +0100

    git

--- html/recommendations.js
+++ html/recommendations.js
@@ -86,6 +86,24 @@ document.addEventListener("DOMContentLoaded", () => {
     // Dohvaćanje preporuka
     let recommendations = getRecommendations(currentPage, userHistory.slice(-3, -1), pages);
     displayRecommendations(recommendations);
+
+
+    function cosineSimilarity(vecA, vecB) {
+        let dotProduct = 0;
+        let normA = 0;
+        let normB = 0;
+    
+        for (let i = 0; i < vecA.length; i++) {
+          dotProduct += vecA[i] * vecB[i];
+          normA += vecA[i] * vecA[i];
+          normB += vecB[i] * vecB[i];
+        }
+    
+        normA = Math.sqrt(normA);
+        normB = Math.sqrt(normB);
+    
+        return dotProduct / (normA * normB);
+      }
   
     // Funkcija za generiranje TF-IDF vektora
     function generateTFIDFVector(content, allContents) {
